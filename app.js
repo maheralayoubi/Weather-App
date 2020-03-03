bb55b42a2130979ee21a05ce5b122ec2ef566ba1
MaHeR <45547826+maheralayoubi@users.noreply.github.com>
2020-03-03 13:01:33 +0900

    Fetching the data form Darksky API with the support of Herokuapp proxy

--- app.js
+++ app.js
@@ -9,6 +9,20 @@ window.addEventListener('load', () => {
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude;
             lat = position.coords.latitude;
+
+            // This API enables cross-origin requests to anywhere.
+            const proxy = 'http://cors-anywhere.herokuapp.com/';
+            const api = `${proxy}https://api.darksky.net/forecast/2e5c7457953c48614814c66834bfa7d3/${lat},${long}`;
+
+            // Get the information from the API
+            fetch(api)
+                .then(response => {
+                    // Convert the info into JSON
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data);
+                });
         });
     }
 });
